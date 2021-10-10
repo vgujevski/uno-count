7640078f7bfa54ec22161a9fa2a0bf735ae71687
Vitali G <gujevski@gmail.com>
2021-10-10 21:57:01 +0300

    adjust styles of score list item on PlayerPage

--- src/features/player-scores/ScoresListItem.js
+++ src/features/player-scores/ScoresListItem.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { useDispatch } from 'react-redux'
 
 import { playerScoreRemoved } from '../players/playersListSlice'
+import closeIcon from '../../images/close_white_48dp.svg'
 
 export const ScoresListItem = (score) => {
 
@@ -9,13 +10,15 @@ export const ScoresListItem = (score) => {
 
   const handleDeleteScoreClick = () => {
     console.log('handleDeleteScoreClick called with id: ', score.scoreId);
-    dispatch(playerScoreRemoved({scoreId: score.scoreId}))
+    dispatch(playerScoreRemoved({ scoreId: score.scoreId }))
   }
 
   return (
     <div className="list__item">
       <span>{score.amount}</span>
-      <button onClick={handleDeleteScoreClick}>x</button>
+      <button className="button dark button__icon" onClick={handleDeleteScoreClick}>
+        <img src={closeIcon} alt="add player" />
+      </button>
     </div>
   )
 }
